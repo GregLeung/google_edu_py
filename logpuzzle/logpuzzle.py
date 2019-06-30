5274187bc9018c93779fa31493589074ce1c6a4f
GregLeung <km0205hk@gmail.com>
2019-06-30 20:47:32 +1000

    finished logpuzzle

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -1,42 +1,48 @@
-#!/usr/bin/python
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
 
 import os
 import re
 import sys
 import urllib
+import urlparse
 
-"""Logpuzzle exercise
-Given an apache logfile, find the puzzle urls and download the images.
+def sort_by_picture_name(url):
+  match = re.search(r'puzzle/(.*)', url)
+  return match.group()
 
-Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
-"""
-
+def sort_by_picture_name_place(url):
+  match = re.search(r'puzzle/.*-.*-(.*).jpg', url)
+  return match.group(1)
 
 def read_urls(filename):
-  """Returns a list of the puzzle urls from the given log file,
-  extracting the hostname from the filename itself.
-  Screens out duplicate urls and returns the urls sorted into
-  increasing order."""
-  # +++your code here+++
+  f = open(filename)
+  urls = re.findall(r'GET\s(.*puzzle/.*.jpg)', f.read())
+  urls = list(dict.fromkeys(urls))
+  urls = sorted(urls, key=sort_by_picture_name_place)
+  filename = filename.split('_')
+  result = []
+  for url in urls:
+    result.append('http://' + filename[1] + url)
+  return result
   
 
 def download_images(img_urls, dest_dir):
-  """Given the urls already in the correct order, downloads
-  each image into the given directory.
-  Gives the images local filenames img0, img1, and so on.
-  Creates an index.html in the directory
-  with an img tag to show each local image file.
-  Creates the directory if necessary.
-  """
-  # +++your code here+++
-  
+  f = open('index.html', 'w')
+  f.write('<html>')
+  f.write('<body>')
+  if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+  count = 0
+  files = ''
+  for url in img_urls:
+    print('Retrieving from: ' + url + '...')
+    fileUrl = './imgs/img' + str(count)
+    urllib.urlretrieve(url, fileUrl)
+    count += 1
+    files += '<img src="' + fileUrl + '">'
+  f.write(files)
+  f.write('</body>')
+  f.write('</html>')
+  f.close()
 
 def main():
   args = sys.argv[1:]
